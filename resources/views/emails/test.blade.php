2565e947a6ea00483d852740ade447e0bc449ecf
Faechtig <benjamin.faechtig@epitech.eu>
2024-07-02 16:51:51 +0200

    feat:notification

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/emails/test.blade.php -->
+<html>
+<body>
+    <h1>Ceci est un email de test</h1>
+    <p>Merci d'utiliser notre application!</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\batteryStatus;
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestEmail;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/battery-lvl', [BatteryStatus::class, 'getBatteryLvL']);
+// Route::get('/send-notification',[BatteryStatus::class, 'sendNotification']);
+
+Route::get('/send-test-email', function () {
+    Mail::to('your_email@example.com')->send(new TestEmail());
+    return 'Email envoyé avec succès!';
 });
