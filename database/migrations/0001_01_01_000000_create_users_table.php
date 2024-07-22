c245877545cf5668eea2a1f8b9ea6e65f4dd3cc9
AlxisHenry <alexis.henry150357@gmail.com>
2024-07-22 10:23:00 +0200

    fix: longText for avatar

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin')->default(false);
-            $table->string('avatar')
+            $table->longText('avatar')
                 ->nullable()
                 ->default(null);
             $table->string('passcode')
