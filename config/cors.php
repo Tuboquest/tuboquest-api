3c7522ffa945d33f3e34c76b17c93083173cc35e
AlxisHenry <alexis.henry150357@gmail.com>
2024-08-27 16:40:08 +0200

    fix: cors allowed origins

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,8 @@ return [
 
     'allowed_origins' => [
         env('FRONTEND_URL', 'http://localhost:3000'),
-        'http://127.0.0.1:8080'
+        'http://127.0.0.1:8080',
+        'https://tuboquest.alexishenry.eu/'
     ],
 
     'allowed_origins_patterns' => [],
