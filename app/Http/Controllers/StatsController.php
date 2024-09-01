4ae5c9f27ba3984b9707b7d568aa3412569d4cf3
AlexisHenry <alexis.henry150357@gmail.com>
2024-09-01 20:47:06 +0200

    feat: stats tests

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -3,24 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StatsController extends Controller
 {
     public function countOfUse()
     {
-        return response()->json([
-            'Janvier' => 100,
-            'Février' => 200,
-            'Mars' => 300,
-            'Avril' => 400,
-            'Mai' => 500,
-            'Juin' => 600,
-            'Juillet' => 700,
-            'Août' => 800,
-            'Septembre' => 900,
-            'Octobre' => 1000,
-            'Novembre' => 1100,
-            'Décembre' => 1200,
-        ]);
+        $user = Auth::user();
+
+        /** @var \App\Models\User $user */
+        $movements = $user->movements();
+
+        $months = $movements->get()->groupBy(function ($movement) {
+            return $movement->created_at->format('F');
+        });
+
+        $countOfUse = $months->map(function ($month) {
+            return $month->count();
+        });
+
+        return response()->json($countOfUse);
     }
 }
